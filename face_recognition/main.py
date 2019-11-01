83afa4123f278fd5697e4be2a686fe01d1e2ff01
Nonthakon Jitchiranant <non_thakon@hotmail.com>
2019-11-01 23:04:33 +0700

    finished face recog

--- face_recognition/main.py
+++ face_recognition/main.py
@@ -1,61 +1,68 @@
 import face_recognition
-# from picamera import PiCamera
-from time import sleep
+import picamera
+import numpy as np
 import requests
 
-# camera = PiCamera()
-URL = "https://warm-lake-39502.herokuapp.com/people"
+camera = picamera.PiCamera()
+camera.resolution = (800, 800)
+output = np.empty((800, 800, 3), dtype=np.uint8)
 
-# Load the known people faces
-biden_image = face_recognition.load_image_file("img\\biden.jpg")
-obama_image = face_recognition.load_image_file("img\\obama.jpg")
-nonthakon_image = face_recognition.load_image_file("img\\nonthakon.jpg")
+URL = "https://warm-lake-39502.herokuapp.com/update_people"
 
-#encoding people faces
-biden_face_encoding = face_recognition.face_encodings(biden_image)[0]
-obama_face_encoding = face_recognition.face_encodings(obama_image)[0]
+print("Loading known face image(s)")
+nonthakon_image = face_recognition.load_image_file("img/nonthakon.jpg")
 nonthakon_face_encoding = face_recognition.face_encodings(nonthakon_image)[0]
+nonthakon2_image = face_recognition.load_image_file("img/nonthakon_test.jpg")
+nonthakon2_face_encoding = face_recognition.face_encodings(nonthakon2_image)[0]
+pbeer_image = face_recognition.load_image_file("img/pbeer.jpg")
+pbeer_face_encoding = face_recognition.face_encodings(pbeer_image)[0]
+prayuth_image = face_recognition.load_image_file("img/prayuth.jpg")
+prayuth_face_encoding = face_recognition.face_encodings(prayuth_image)[0]
 
-#make a list of people faces encoding
 known_faces = [
-    biden_face_encoding,
-    obama_face_encoding,
-    nonthakon_face_encoding
+    nonthakon_face_encoding,
+    nonthakon2_face_encoding,
+    pbeer_face_encoding,
+    prayuth_face_encoding
 ]
 
-#list of people name TODO: change to id
-people_names = [
-    "Joe Biden",
-    "Barack Obama",
-    "Nonthakon Jitchiranant"
+# Initialize some variables
+face_locations = []
+face_encodings = []
+
+names = [
+    "Non",
+    "Non",
+    "P'Beer",
+    "Uncle Prayuth",
+    "Stranger",
 ]
 
-#capture picture from pi camera save as a file
-#encoding the picture from camera
-#make a results list then loop in it if the idex is True send request to update the database
+while True:
+    print("Capturing image.")
+    # Grab a single frame of video from the RPi camera as a numpy array
+    camera.capture(output, format="rgb")
 
-while(True):
-    # camera.start_preview()
-    # camera.capture('/home/pi/Desktop/image.jpg')
-    # camera.stop_preview()
-    # unknown_image = face_recognition.load_image_file("/home/pi/Desktop/image.jpg")
-    unknown_image = face_recognition.load_image_file("img\\stranger.jpg")
-    try:
-        unknown_face_encoding = face_recognition.face_encodings(unknown_image)[0]
-        results = face_recognition.compare_faces(known_faces, unknown_face_encoding)
+    # Find all the faces and face encodings in the current frame of video
+    face_locations = face_recognition.face_locations(output)
+    print("Found {} faces in image.".format(len(face_locations)))
+    face_encodings = face_recognition.face_encodings(output, face_locations)
 
-        for i in range(len(people_names)):
-            if(results[i]):
-                print(people_names[i])
-                r = requests.put(url = URL, data = {'name': people_names[i]})
+    # Loop over each face found in the frame to see if it's someone we know.
+    for face_encoding in face_encodings:
+        # See if the face is a match for the known face(s)
+        matches = face_recognition.compare_faces(known_faces, face_encoding)
+        name = ""
 
-        if (True not in results):
-            # send a request there's stranger
-            print("Found no one")
-            r = requests.put(url = URL, data = {'name': 'unknown' })
-        
-        sleep(5)
-    except Exception as e:
-        print(e)
-        
-        sleep(5)
+        for match in matches:
+            if match:
+                name = names[matches.index(match)]
+                r = requests.post(url = URL, data = {'name': names[matches.index(match)]})
+                
+        if True not in matches:
+            #print("Stranger!")
+            r = requests.post(url = URL, data = {'name': names[5]})
+        else:
+            print("I see someone named {}!".format(name))
+
+
